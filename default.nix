3b02dec6fa34cfeb6ff2ac95cbca828b2443cd32
BigDen <d.iljn.nkl@gmail.com>
2025-04-10 12:40:04 +0000

    insanity 2

--- default.nix
+++ default.nix
@@ -1,10 +1,12 @@
 let
   pkgs = import <nixpkgs> { };
+  callPackage = pkgs.lib.callPackageWith (pkgs // packages);
+  packages = {
+    a = callPackage ./a.nix { };
+    b = callPackage ./b.nix { };
+    c = callPackage ./c.nix { };
+    d = callPackage ./d.nix { };
+    e = callPackage ./e.nix { };
+  };
 in
-rec {
-  a = pkgs.callPackage ./a.nix { };
-  b = pkgs.callPackage ./b.nix { inherit a; };
-  c = pkgs.callPackage ./c.nix { inherit b; };
-  d = pkgs.callPackage ./d.nix { };
-  e = pkgs.callPackage ./e.nix { inherit c d; };
-}
+packages
